7fd7e8ebb6cb4018c0595bcf011a888ecdfa8365
Vladimir Osintsev <oc@co.ru>
2016-11-01 22:26:02 +0300

    Minor fixes

--- asyncwebo2/settings.py
+++ asyncwebo2/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'asyncwebo2.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.config(default='postgres://postgres@localhost:5432/postgres')
 }
 
 
@@ -118,10 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
-# Append slas
+# Append slash
 
 APPEND_SLASH = True
 
